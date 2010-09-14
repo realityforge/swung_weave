250fde0612dd2dc4b395278ea728ce0e6b6393e8
Behrang Saeedzadeh <behrangsa@gmail.com>
2010-09-14 15:44:42 +1000

    Added a failing DummyTest to see how it affects Hudon

---
+++ api/src/test/java/org/realityforge/swung_weave/DummyTest.java
@@ -0,0 +1,16 @@
+package org.realityforge.swung_weave;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ *
+ */
+public class DummyTest
+{
+  @Test
+  public void failingTest()
+  {
+    Assert.assertEquals( true, false );
+  }
+}
